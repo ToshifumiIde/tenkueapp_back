094d7485bef21e83ef8c269096d7dd1057b656dd
ToshifumiIde <toshifumiide198765@gmail.com>
2020-10-19 21:34:55 +0900

    HEADERとROOMの修正

--- src/asset/js/app.js
+++ src/asset/js/app.js
@@ -2,18 +2,17 @@
 
 {
     const nav = document.getElementById('hamburgerMenu');
-    const hamburgerLines = document.querySelectorAll('.hamburger-line');
-    const navMenu = document.querySelector('.nav-toggle-menu');
-    const navLists = document.querySelectorAll('.nav-toggle-menu .header-nav__list');
+    const hamburgerLines = document.querySelectorAll('.l-hamburger__line');
+    const navMenu = document.querySelector('.l-nav__toggle__menu');
+    const navLists = document.querySelectorAll('.l-nav__toggle__menu .l-header-nav__list');
 
     nav.addEventListener('click', () => {
         hamburgerLines.forEach(hamLine => {
-            hamLine.classList.toggle('clicked');
+            hamLine.classList.toggle('is-clicked');
         });
         navLists.forEach(navList => {
-            navList.classList.toggle('clicked');
+            navList.classList.toggle('is-clicked');
         });
-
-        navMenu.classList.toggle('clicked');
+        navMenu.classList.toggle('is-clicked');
     });
 }
